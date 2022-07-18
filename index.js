0d9bd0ff39ab9c56fe70c6d0aceef4a8d9b132e4
+ <+>
2022-07-18 16:29:10 +1000

    commit tree 9e0e31a26c502966c936266d4d6aec3830f91295

--- index.js
+++ index.js
@@ -1,2 +1,17 @@
 
-// Write your code here
+// an equation whose multiplied results will equal 62
+
+const num1 = 2; 
+const num2 = 31;
+const multiply = num1 * num2 
+
+// a random integer greater than 0
+const random = Math.floor(Math.random() * 1) + 1
+
+//an equation that calculates a remainder that is equal to 4 
+const num3 = 16;
+const num4 = 6;
+const mod = num3 % num4;
+
+// will return 20 as the highest number in the set
+const max = Math.max(1, 2, 3, 4, 20);
